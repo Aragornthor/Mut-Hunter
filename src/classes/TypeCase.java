890608cac755fe49714cff2e2daa1ba1022bc376
MrKeesLer <bankaert.benoit@gmail.com>
2019-03-27 11:11:10 +0100

    Ajout de la javadoc de TypeCase

--- src/classes/TypeCase.java
+++ src/classes/TypeCase.java
@@ -1,6 +1,12 @@
 package classes;
 
+/**
+ * Cette classe à pour but de définir les différents type de case avec leurs caractères.
+ * @author Bankaert Benoit
+ * @version 1.0
+ */
 public enum TypeCase {
+
 	NORMAL(' '),LOOT('O'),PORTAIL('P'),MONSTRE('M'),MONSTRE_HIDE(' '),CHASSEUR('C'),CHASSEUR_HIDE(' ');
 	
 	private char icon;
@@ -9,6 +15,10 @@ public enum TypeCase {
 		this.icon = icon;
 	}
 	
+	/**
+	 * 
+	 * @return le caractere du type.
+	 */
 	public char getIcon() {
 		return this.icon;
 	}
