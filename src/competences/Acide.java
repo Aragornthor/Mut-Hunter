4892f760df07443f2c68767b2dc9d731c103e464
Ataron <ataron205@gmail.com>
2019-03-27 10:57:34 +0100

    JavaDoc pour le classes competences + loot

--- src/competences/Acide.java
+++ src/competences/Acide.java
@@ -1,5 +1,12 @@
 package competences;
 
+
+/**
+ * 
+ * La classe Acide est une competence du monstre faisant perdre de l'energie au chasseur pendant 3 tours
+ * @author Quentin Delmarre, Robin Gallifa
+ *
+ */
 public class Acide implements Competences {
 	private int id = 0;
 	private String element;
@@ -12,27 +19,51 @@ public class Acide implements Competences {
 		this.duree = 3;
 	}
 
+	/**
+	 * 
+	 * @return Retourne l'element de la competence
+	 */
 	public String getElement() {
 		return element;
 	}
 
+	/**
+	 * 
+	 * @return Retourne l'effet de la competence
+	 */
 	public int getEffet() {
 		return effet;
 	}
 
+	/**
+	 * 
+	 * @return Retourne la duree de la competence
+	 */
 	public int getDuree() {
 		return duree;
 	}
 	
+	/**
+	 * 
+	 * @return Retourne l'id de la competence
+	 */
 	public int getId() {
 		return id;
 	}
 	
+	/**
+	 * 
+	 * @return Retourne sous forme textuelle la competence 
+	 */
 	@Override
 	public String toString() {
 		return "["+this.element+":"+this.effet+":"+this.duree+"]";
 	}
 
+	/**
+	 * 
+	 * @return Retourne vrai si la competence donnee en parametre est la meme que celle de la classe actuelle, sinon faux
+	 */
 	@Override
 	public boolean equals(Competences c) {
 		return this.id == c.getId();
--- src/competences/Piege.java
+++ src/competences/Piege.java
@@ -1,5 +1,11 @@
 package competences;
 
+/**
+ * 
+ * La classe Piege est une competence chasseur permettant de creer un piege
+ * @author Quentin Delmarre, Robin Gallifa
+ *
+ */
 public class Piege implements Competences {
 	private int id = 3;
 	private String element;
@@ -12,27 +18,51 @@ public class Piege implements Competences {
 		this.duree = 0;
 	}
 
+	/**
+	 * 
+	 * @return Retourne l'element de la competence
+	 */
 	public String getElement() {
 		return element;
 	}
 
+	/**
+	 * 
+	 * @return Retourne l'effet de la competence
+	 */
 	public int getEffet() {
 		return effet;
 	}
 
+	/**
+	 * 
+	 * @return Retourne la duree de la competence
+	 */
 	public int getDuree() {
 		return duree;
 	}
 	
+	/**
+	 * 
+	 * @return Retourne l'id de la competence
+	 */
 	public int getId() {
 		return id;
 	}
 	
+	/**
+	 * 
+	 * @return Retourne sous forme textuelle la competence 
+	 */
 	@Override
 	public String toString() {
 		return "["+this.element+":"+this.effet+":"+this.duree+"]";
 	}
 	
+	/**
+	 * 
+	 * @return Retourne vrai si la competence donnee en parametre est la meme que celle de la classe actuelle, sinon faux
+	 */
 	@Override
 	public boolean equals(Competences c) {
 		return this.id == c.getId();
