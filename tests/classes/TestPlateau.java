1a66033e3ac53472d19e980488502c08dc986db0
MrKeesLer <bankaert.benoit@gmail.com>
2019-04-03 09:07:34 +0200

    Fix classe testPlateau

--- tests/classes/TestPlateau.java
+++ tests/classes/TestPlateau.java
@@ -10,7 +10,6 @@ public class TestPlateau {
 		//System.out.println("HI");
 		Personnage p = new Personnage("chasseur",new Position(9,9));
 		Personnage p1 = new Personnage("monstre",new Position(0,0));
-		Position p2 = new Position(9,9);
 		
 		
 		Plateau pl = new Plateau();
@@ -18,74 +17,6 @@ public class TestPlateau {
 		pl.startPersonnage(p, p1);
 		pl.ajoutLoot(3);
 		pl.affichePlateau(p);
-		System.out.println("Début de la partie !!");
-		do {
-			//System.out.println(p1.toString()+"\n"+p2.toString());
-			deplacementChasseur(pl,p);
-			pl.affichePlateau(p);
-			pl.addTours();
-		}while(!pl.victoireChasseur(p.getPosition(),p2));
-		System.out.println("Fin de la partie !!");
 	}
 	
-	public static void deplacementChasseur(Plateau pl,Personnage p) {
-		String choix = "";
-		do {
-			System.out.print("Direction :");
-			choix = sc.nextLine();
-		}while(!choix.equals("z") && !choix.equals("s") && !choix.equals("q") && !choix.equals("d"));
-		switch(choix) {
-		case "z":
-			pl.setCaseType(p.getPosition(), TypeCase.NORMAL);
-			p.getPosition().setX(p.getPosition().getX()-1);
-			if(p.getPosition().getX() < 0) {
-				p.getPosition().setX(p.getPosition().getX()+1);
-				System.out.println("Sortie du terrain !");
-				deplacementChasseur(pl,p);
-				break;
-			}
-			if(pl.getCase(p.getPosition()).getEstPortail()) p.setPosition(pl.teleportation(p.getPosition())); 
-			if(pl.getCase(p.getPosition()).getLoot()) pl.ajoutCompetence(p);
-			pl.setCaseType(p.getPosition(), TypeCase.CHASSEUR);
-			break;
-		case "s":
-			pl.setCaseType(p.getPosition(), TypeCase.NORMAL);
-			p.getPosition().setX(p.getPosition().getX()+1);
-			if(p.getPosition().getX() >= pl.getHauteur()) {
-				p.getPosition().setX(p.getPosition().getX()-1);
-				System.out.println("Sortie du terrain !");
-				deplacementChasseur(pl,p);
-				break;
-			}
-			if(pl.getCase(p.getPosition()).getEstPortail()) p.setPosition(pl.teleportation(p.getPosition()));
-			if(pl.getCase(p.getPosition()).getLoot()) pl.ajoutCompetence(p);
-			pl.setCaseType(p.getPosition(), TypeCase.CHASSEUR);
-			break;
-		case "q":
-			pl.setCaseType(p.getPosition(), TypeCase.NORMAL);
-			p.getPosition().setY(p.getPosition().getY()-1);
-			if(p.getPosition().getY() < 0) {
-				p.getPosition().setY(p.getPosition().getY()+1);
-				System.out.println("Sortie du terrain !");
-				deplacementChasseur(pl,p);
-				break;
-			}
-			if(pl.getCase(p.getPosition()).getEstPortail()) p.setPosition(pl.teleportation(p.getPosition()));
-			if(pl.getCase(p.getPosition()).getLoot()) pl.ajoutCompetence(p);
-			pl.setCaseType(p.getPosition(), TypeCase.CHASSEUR);
-			break;
-		case "d":
-			pl.setCaseType(p.getPosition(), TypeCase.NORMAL);
-			p.getPosition().setY(p.getPosition().getY()+1);
-			if(p.getPosition().getY() >= pl.getLargeur()) {
-				p.getPosition().setY(p.getPosition().getY()-1);
-				System.out.println("Sortie du terrain !");
-				deplacementChasseur(pl,p);
-				break;
-			}
-			if(pl.getCase(p.getPosition()).getLoot()) p.setPosition(pl.teleportation(p.getPosition())); 
-			pl.setCaseType(p.getPosition(), TypeCase.CHASSEUR);
-			break;		
-		}
-	}
 }
