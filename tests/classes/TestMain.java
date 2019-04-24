dcce9df7feb9a97762d6be0822063d3cd56d2791
Quentin DELMARRE <delmarrq@ayou05.iut-infobio.priv.univ-lille1.fr>
2019-04-24 09:11:34 +0200

    Décommenter les hide

--- tests/classes/TestMain.java
+++ tests/classes/TestMain.java
@@ -3,7 +3,7 @@ package classes;
 public class TestMain {
 	
 	static Plateau jeu = new Plateau();
-	static Personnage monstre = new Monstre(new Position(9,9));
+	static Personnage monstre = new IAMonstre(new Position(9,9));
 	static Personnage chasseur = new Chasseur(new Position(0,0));
 	
 	public static void main(String[] args) {
@@ -27,7 +27,7 @@ public class TestMain {
 
 	public static boolean tourChasseur() {
 		jeu.getCase(chasseur.getPosition()).show(); //Montre le chasseur
-		//jeu.getCase(monstre.getPosition()).hide();	//et cache le monstre
+		jeu.getCase(monstre.getPosition()).hide();	//et cache le monstre
 		int deplacementsRestant = chasseur.getDeplacement();
 		
 		while(deplacementsRestant>0) {
@@ -47,7 +47,7 @@ public class TestMain {
 	
 	public static boolean tourMonstre() {
 		jeu.getCase(monstre.getPosition()).show();	//Montre le monstre
-		//jeu.getCase(chasseur.getPosition()).hide();	//et cache le chasseur
+		jeu.getCase(chasseur.getPosition()).hide();	//et cache le chasseur
 		boolean perdu = false;
 		
 		int deplacementsRestant = monstre.getDeplacement();
