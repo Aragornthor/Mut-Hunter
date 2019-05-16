156f0b26b2c6086c51acd2c05979ebf276153614
MrKeesLer <bankaert.benoit@gmail.com>
2019-05-16 10:51:00 +0200

    Legère modif de PlayerInfo

--- src/UI/PlayerInfo.java
+++ src/UI/PlayerInfo.java
@@ -31,6 +31,8 @@ public class PlayerInfo extends Application{
 		
 	}
 	
+	
+	
 	public Scene getScene() {
 		GridPane playerInfo = new GridPane();
 		
@@ -38,37 +40,16 @@ public class PlayerInfo extends Application{
 		GraphicsContext gc = playerIcon.getGraphicsContext2D();
 		gc.setFill(Color.ORANGE);
 		gc.fillRect(0, 0, 100, 100);
-		Slider energy = new Slider(0,100,75);
-		Label valueEnergy = new Label("Energie : "+(int)energy.getValue()+"/100");
-		Button comp1 = new Button("Competences 1");
-		Button comp2 = new Button("Competences 2");
+		Label valueEnergy = new Label();
 		Label infoTitle = new Label("Info : ");
 		TextArea info = new TextArea();
-		info.setEditable(false);
+		info.setEditable(false);	
 		
-		comp1.addEventHandler(MouseEvent.MOUSE_ENTERED, e ->{
-			infoTitle.setText("Info : Comp1");
-			info.setText("Ceci est un test");
-		});
 		
-		comp1.addEventHandler(MouseEvent.MOUSE_EXITED, e ->{
-			infoTitle.setText("Info : ");
-			info.setText("");
-		});
+		Slider energy = getSlider(valueEnergy);
+		Button comp1 = getComp1(infoTitle,info);
+		Button comp2 = getComp2(infoTitle,info);	
 		
-		comp2.addEventHandler(MouseEvent.MOUSE_ENTERED, e ->{
-			infoTitle.setText("Info : Comp2");
-			info.setText("Ceci est un test");
-		});
-		
-		comp2.addEventHandler(MouseEvent.MOUSE_EXITED, e ->{
-			infoTitle.setText("Info : ");
-			info.setText("");
-		});
-		
-		energy.addEventHandler(MouseEvent.MOUSE_DRAGGED, e ->{
-			valueEnergy.setText("Energie : "+(int)energy.getValue()+"/100");
-		});
 		
 		GridPane.setMargin(playerIcon, new Insets(5));
 		GridPane.setMargin(energy, new Insets(5));
@@ -89,6 +70,46 @@ public class PlayerInfo extends Application{
 		return new Scene(playerInfo,720,250);
 	}
 	
+	
+	
+	private Button getComp1(Label infoTitle,TextArea info) {
+		Button comp1 = new Button("Competences 1");
+		comp1.addEventHandler(MouseEvent.MOUSE_ENTERED, e ->{
+			infoTitle.setText("Info : Comp1");
+			info.setText("Ceci est un test");
+		});
+		
+		comp1.addEventHandler(MouseEvent.MOUSE_EXITED, e ->{
+			infoTitle.setText("Info : ");
+			info.setText("");
+		});
+		return comp1;
+	}
+	
+	
+	
+	private Button getComp2(Label infoTitle,TextArea info) {
+		Button comp2 = new Button("Competences 2");
+		comp2.addEventHandler(MouseEvent.MOUSE_ENTERED, e ->{
+			infoTitle.setText("Info : Comp2");
+			info.setText("Ceci est un test");
+		});
+		
+		comp2.addEventHandler(MouseEvent.MOUSE_EXITED, e ->{
+			infoTitle.setText("Info : ");
+			info.setText("");
+		});
+		return comp2;
+	}
+	
+	private Slider getSlider(Label valueEnergy) {
+		Slider energy = new Slider(0,100,75);
+		valueEnergy.setText("Energie : "+(int)energy.getValue()+"/100");
+		energy.addEventHandler(MouseEvent.MOUSE_DRAGGED, e ->{
+			valueEnergy.setText("Energie : "+(int)energy.getValue()+"/100");
+		});
+		return energy;
+	}
 	
 	
 	 
