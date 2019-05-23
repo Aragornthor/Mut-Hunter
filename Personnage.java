f1d2d98ecfd85023e0a3e7cadc5b0f382e19c521
ElDruinXa <48836075+ElDruinXa@users.noreply.github.com>
2019-05-23 09:38:06 +0200

    Fix deplacement + affichage
    Les déplacements fonctionnent désormais normalement

---
+++ Personnage.java
@@ -0,0 +1,339 @@
+package classes;
+
+import java.util.Scanner;
+
+import competences.Competences;
+import competences.Statut;
+import javafx.scene.image.Image;
+
+/**
+ * 
+ * La classe Personnage est une classe abstraite regroupant les attributs et les méthodes communs aux personnages
+ * @author Quentin Delmarre, Xavier Lezzoche, Robin Gallifa
+ *
+ */
+public abstract class Personnage {
+	static Scanner reader = new Scanner(System.in);
+	private String type;
+	private int energie;
+	private int maxEnergie;
+	private Position position;
+	private Competences[] competences;
+	private int deplacement;
+	private int vie;
+	private Statut statut;
+	
+	/**
+	 * Constructeur du Personnage avec des paramètres prédéfinis
+	 * @param type Prend en paramètre le type du personnage
+	 * @param p Prend en paramètre la Position du personnage
+	 */
+	public Personnage(Position p) {
+		this.energie = 75;
+		this.position = p;
+		this.deplacement = 3;
+		this.vie = 1;
+		this.maxEnergie = 100;
+		this.statut = Statut.Vivant;
+	}
+	
+	/**
+	 * 
+	 * @return Retourne le niveau d'énergie du personnage
+	 */
+	public int getEnergie() {
+		return energie;
+	}
+	
+	abstract public Image getImage();
+
+	/**
+	 * 
+	 * @return Retourne le niveau maximum du personnage
+	 */
+	public int getMaxEnergie() {
+		return maxEnergie;
+	}
+
+	/**
+	 * 
+	 * @return Retourne la position du personnage sur le Plateau
+	 */
+	public Position getPosition() {
+		return position;
+	}
+	
+	/**
+	 * Remplace la position du joueur par la position passé en parametre.
+	 * @param p est la nouvelle position
+	 */
+	public void setPosition(Position p) {
+		this.position.setX(p.getX());
+		this.position.setY(p.getY());
+	}
+
+	/**
+	 * 
+	 * @return Retourne la liste de compétences du personnage
+	 */
+	public Competences[] getCompetences() {
+		return competences;
+	}
+
+	/**
+	 * 
+	 * @return Retourne la vitesse de déplacement du personnage
+	 */
+	public int getDeplacement() {
+		return deplacement;
+	}
+
+	/**
+	 * 
+	 * @return Retourne le statut vitale du personnage
+	 */
+	public int getVie() {
+		return vie;
+	}
+	
+	/**
+	 * Permet de redemander un deplacement tant que ce que donne le joueur n'est pas bon
+	 * @param p Prend un plateau en paramètre
+	 */
+	public abstract void seDeplace(Plateau p);
+	
+	/**
+	 * Permet au joueur d'entrer son déplacement(zqsd)
+	 * @param p Prend un plateau en paramètre
+	 * @return Retourne VRAI si le mouvement est effectué, sinon FAUX
+	 */
+	protected boolean estDeplaceJoueur(Plateau p) {
+		String entree = reader.nextLine();
+		return estDeplace(p, entree);
+	}
+	
+	/**
+	 * Permet un déplacement
+	 * @param p Prend un plateau en paramètre
+	 * @return Retourne VRAI si le mouvement est effectué, sinon FAUX
+	 */
+	public boolean estDeplace(Plateau p, String entree) {
+		
+		//System.out.println("Debug estDeplace(...)");
+		Position pos = new Position(this.position.getX(), this.position.getY());
+		int deplacement = this.deplacement;
+		boolean reussite = false;
+		
+		if(entree.equalsIgnoreCase("z")) { //SE DEPLACE VERS LE HAUT
+			if(this.position.getY()-1<0) {
+				reussite = false;
+			} else {
+				//System.out.println("Z detect");
+				p.setCaseNormal(this.position);
+				this.position.setY(this.position.getY()-1);
+				this.deplacement -= p.getCase(this.getPosition()).getTypeTerrain().getDeplacement();
+				if(p.getCase(this.position).getEstPortail()) this.setPosition(p.teleportation(this.position));
+				else if(p.getCase(this.getPosition()).getLoot()) {
+					p.ajoutCompetence(this);
+					p.setDernierLoot();
+				}
+				else if(p.getCase(this.getPosition()).getEstPiege() && this.type.equalsIgnoreCase("monstre")) {
+					if(!this.getStatut().equals(Statut.Shield)) {
+						this.setStatut(Statut.Stun);
+						this.getStatut().setTour(2);
+					}
+					p.getCase(this.getPosition()).setTypeCase(TypeCase.VIDE);
+				}
+				changeCase(p);
+				reussite = true;
+			}
+		}
+		
+		if(entree.equalsIgnoreCase("s")) { //SE DEPLACE VERS LE BAS
+			if(this.position.getY()+1>= p.getHauteur()) {
+				reussite = false;
+			} else {
+				//System.out.println("S detect");
+				p.setCaseNormal(this.position);
+				this.position.setY(this.position.getY()+1);
+				this.deplacement -= p.getCase(this.getPosition()).getTypeTerrain().getDeplacement();
+				if(p.getCase(this.position).getEstPortail()) this.setPosition(p.teleportation(this.position));
+				else if(p.getCase(this.getPosition()).getLoot()) {
+					p.ajoutCompetence(this);
+					p.setDernierLoot();
+				}
+				else if(p.getCase(this.getPosition()).getEstPiege() && this.type.equalsIgnoreCase("monstre")) {
+					if(!this.getStatut().equals(Statut.Shield)) {
+						this.setStatut(Statut.Stun);
+						this.getStatut().setTour(2);
+					}
+					p.getCase(this.getPosition()).setTypeCase(TypeCase.VIDE);
+				}
+				changeCase(p);
+				reussite = true;
+			}
+		}
+		
+		if(entree.equalsIgnoreCase("q")) { //SE DEPLACE VERS LA GAUCHE
+			if(this.position.getX()-1<0) {
+				reussite = false;
+			} else {
+				//System.out.println("Q detect");
+				p.setCaseNormal(this.position);
+				this.position.setX(this.position.getX()-1);
+				this.deplacement -= p.getCase(this.getPosition()).getTypeTerrain().getDeplacement();
+				if(p.getCase(this.position).getEstPortail()) this.setPosition(p.teleportation(this.position));
+				else if(p.getCase(this.getPosition()).getLoot()) {
+					p.ajoutCompetence(this);
+					p.setDernierLoot();
+				}
+				else if(p.getCase(this.getPosition()).getEstPiege() && this.type.equalsIgnoreCase("monstre")) {
+					if(!this.getStatut().equals(Statut.Shield)) {
+						this.setStatut(Statut.Stun);
+						this.getStatut().setTour(2);
+					}
+					p.getCase(this.getPosition()).setTypeCase(TypeCase.VIDE);
+				}
+				changeCase(p);
+				reussite = true;
+			}
+		}
+		
+		if(entree.equalsIgnoreCase("d")) { //SE DEPLACE VERS LA DROITE
+			if(this.position.getX()+1>=p.getLargeur()) {
+				reussite = false;
+			} else {
+				//System.out.println("D detect");
+				p.setCaseNormal(this.position);
+				this.position.setX(this.position.getX()+1);
+				this.deplacement -= p.getCase(this.getPosition()).getTypeTerrain().getDeplacement();
+				if(p.getCase(this.position).getEstPortail()) this.setPosition(p.teleportation(this.position));
+				if(p.getCase(this.getPosition()).getLoot()) {
+					p.ajoutCompetence(this);
+					p.setDernierLoot();
+				}
+				else if(p.getCase(this.getPosition()).getEstPiege() && this.type.equalsIgnoreCase("monstre")) {
+					if(!this.getStatut().equals(Statut.Shield)) {
+						this.setStatut(Statut.Stun);
+						this.getStatut().setTour(2);
+					}
+					p.getCase(this.getPosition()).setTypeCase(TypeCase.VIDE);
+				}
+				changeCase(p);
+				reussite = true;
+			}
+		}
+		if(this.deplacement<0) {
+			p.setCaseNormal(this.position);
+			System.out.println("Vous ne pouvez pas vous déplacer ici, vous manquez de points de déplacements");
+			this.deplacement = deplacement;
+			this.setPosition(pos);
+			changeCase(p);
+		}
+		return reussite;
+	}
+	
+	/**
+	 * Change la case où se trouve le personnage
+	 * @param p Prend un plateau en paramètre
+	 */
+	public abstract boolean changeCase(Plateau p);
+
+	public void rechargeEnergie() {
+		rechargeEnergie(10);
+	}
+	
+	/**
+	 * 
+	 * @param i Prend en paramètre la quantité d'énergie à ajouter
+	 */
+	public void rechargeEnergie(int i) {
+		if((this.energie + i) > maxEnergie) this.energie = maxEnergie;
+		else this.energie += i;
+	}
+
+	/**
+	 * 
+	 * @return Retourne le type du personnage
+	 */
+	public String getType() {
+		return type;
+	}
+	
+	public void setTableauCompetences(Competences[] comps) {
+		this.competences = comps;
+	}
+	
+	/**
+	 * Permet de changer l'une des deux competences du personnage avec la competence passee en parametre
+	 * @param comp Competence que l'on veut mettre pour le personnage
+	 * @param nb Nombre de la competence dans le tableau (soit 0 ou 1)
+	 */
+	public void setCompetence(Competences comp, int nb) {
+		this.competences[nb] = comp;
+	}
+	
+	/**
+	 * @return le joueur en version texte
+	 */
+	public String toString() {
+		String res = this.type+" : "+this.energie+"/"+this.maxEnergie+"\nCompetences : \n";
+		for(Competences c:this.competences) {
+			if(c != null) res += c.toString()+"\n";
+		}
+		res+="Statut : "+this.statut.toString();
+		return res;
+	}
+	
+	/**
+	 * @return le type du joueur soit monstre soit chasseur
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public Statut getStatut() {
+		return this.statut;	
+	}
+	
+	public void setStatut(Statut s) {
+		this.statut = s;
+	}
+	
+	public void setDeplacement(int d) {
+		this.deplacement = d;
+	}
+	
+	public void resetMouvement() {
+		this.deplacement = 3;
+	}
+	
+	public boolean gestionStatuts() {
+		if(this.getStatut() == Statut.Stun) {
+			this.getStatut().setTour(this.getStatut().getNbTour()-1);
+			if(this.getStatut().getNbTour() == -1) {
+				this.setStatut(Statut.Vivant);
+				this.getStatut().setTour(0);
+			}
+			return false;
+		}
+		else if(this.getStatut() == Statut.Acide) {
+			this.rechargeEnergie(-10);
+			this.getStatut().setTour(this.getStatut().getNbTour()-1);
+			if(this.getStatut().getNbTour() == -1) {
+				this.setStatut(Statut.Vivant);
+				this.getStatut().setTour(0);
+			}
+		}
+		else if(this.getStatut() == Statut.Shield) {
+			this.getStatut().setTour(this.getStatut().getNbTour()-1);
+			if(this.getStatut().getNbTour() == -1) {
+				this.setStatut(Statut.Vivant);
+				this.getStatut().setTour(0);
+			}
+		}
+		return true;
+	}
+	
+	
+}
---
+++ Plateau.java
@@ -0,0 +1,344 @@
+package classes;
+
+import java.util.Random;
+/**
+ * Cette classe a pour but de gérer et manipuler un terrain, représenter par un tableau à deux dimensions.
+ * @author Bankaert Benoit
+ * @version 1.0
+ */
+public class Plateau {
+	
+	/**
+	 * Rand permet de générer des nombres aléatoires.
+	 */
+	private static Random rand = new Random();
+	/**
+	 * plateau est un tableau à deux dimensions de Case qui représente le terrain de jeu.
+	 */
+	private Case[][] plateau;
+	/**
+	 * tours représente le nombre de tours dans la partie en cours.
+	 */
+	private int tours;
+	/**
+	 * PORTAIL1 représente la position dans le terrain du premier portail.
+	 */
+	private final Position PORTAIL1 = new Position(9,0);
+	/**
+	 * PORTAIL2 représente la position dans le terrain du second portail.
+	 */
+	private final Position PORTAIL2 = new Position(0,9);
+	/**
+	 * tourChasseur indique si c'est au chasseur de jouer.
+	 */
+	private boolean tourChasseur;
+	/**
+	 * tourMonstre indique si c'est au monstre de jouer.
+	 */
+	private boolean tourMonstre;
+	
+	private int dernierLoot;
+	
+	private final int nbCases;
+	
+	private int compteurCasesDecouvertes;
+	
+	/**
+	 * On instancie un terrain de 10 par 10 et on règle le nombre de tour sur 1.
+	 */
+	public Plateau() {
+		this.plateau = new Case[10][10];
+		this.tours = 1;
+		this.dernierLoot = 0;
+		this.nbCases = this.getHauteur()*this.getLargeur()-2;
+		this.compteurCasesDecouvertes = 1;
+	}
+	
+	/**
+	 * Permet d'initialise les cases de tout le terrain et on place les deux joueurs et les deux portails.
+	 */
+	public void initPlateau() {
+		for(int i = 0; i < this.plateau.length; i++) {
+			for(int j = 0; j < this.plateau[i].length; j++) {
+				int entierRandom = rand.nextInt(100);
+				if(entierRandom > 60) {
+					this.plateau[i][j] = new Case(TypeTerrain.PLAINE);
+				} else if(entierRandom > 30){
+					this.plateau[i][j] = new Case(TypeTerrain.FORET);
+				} else if(entierRandom > 15) {
+					this.plateau[i][j] = new Case(TypeTerrain.MONTAGNE);
+				} else {
+					this.plateau[i][j] = new Case(TypeTerrain.EAU);
+				}
+			}
+		}
+		//System.out.println("Fin init");
+		this.generePortail();
+		
+	}
+	public void startPersonnage(Personnage chasseur, Personnage monstre) {
+		
+		this.plateau[chasseur.getPosition().getX()][chasseur.getPosition().getY()].setEstChasseur(true);
+		this.plateau[monstre.getPosition().getX()][monstre.getPosition().getY()].setEstMonstre(true);
+		this.plateau[monstre.getPosition().getX()][monstre.getPosition().getY()].decouvrirCase();
+	}
+	
+	private void generePortail() {
+		this.plateau[this.PORTAIL1.getX()][this.PORTAIL1.getY()].setEstPortail(true);
+		this.plateau[this.PORTAIL2.getX()][this.PORTAIL2.getY()].setEstPortail(true);
+		//System.out.println("Fin gen");
+	}
+	
+	/**
+	 * Permet d'afficher le terrain.
+	 * @param p prend un personnage en paramètre
+	 */
+	public void affichePlateau(Personnage p) {
+		clearScreen();
+		System.out.print("═════════════════════════════════════════\nTour n°"+this.tours+"\n╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗\n║");
+		for(int i = 0; i < this.plateau.length; i++) {
+			for(int j = 0; j < this.plateau[i].length; j++) {
+				if(p.getType().equals("monstre") && this.plateau[i][j].getIcon()==' ' && this.plateau[i][j].getEstDecouvert()) System.out.print(" X ║");
+				else System.out.print(" "+this.plateau[i][j].getIcon()+" ║");
+			}
+			if(i+1 == this.plateau.length) System.out.print("\n╚═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╝\n");
+			else System.out.print("\n╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n║");
+		}
+		System.out.println(p.toString());
+		
+		if(this.plateau[p.getPosition().getX()][p.getPosition().getY()].getEstChasseur()) {
+			if(this.plateau[p.getPosition().getX()][p.getPosition().getY()].getEstDecouvert()) {
+				System.out.println("Case découverte au tour "+this.plateau[p.getPosition().getX()][p.getPosition().getY()].getTempsDecouvert());
+			}else {
+				System.out.println("Case non découverte");
+			}
+		}
+		
+		System.out.println("\n═════════════════════════════════════════");
+	}
+	
+	/**
+	 * 
+	 * @return le terrain.
+	 */
+	public Case[][] getPlateau(){
+		return this.plateau;
+	}
+	 /**
+	  * Remplace le terrain par le terrain passé en parametre.
+	  * @param tab est le nouveau terrain.
+	  */
+	public void setPlateau(Case[][] tab) {
+		this.plateau = tab;
+	}
+	
+	private int nbLoot() {
+		int nb = 0;
+		for(int i = 0; i < this.plateau.length; i++)
+			for(int j = 0; j < this.plateau[i].length; j++)
+				if(this.plateau[i][j].getLoot())
+					nb++;
+		return nb;
+	}
+	
+	/**
+	 * Ajoute de manière aleatoire un nombre de loot passé en parametre sur le terrain.
+	 * @param loot est le nombre de loot que l'on souhaite ajouter.
+	 */
+	public void ajoutLoot(int loot) {
+		if(loot+this.nbLoot() > 2) loot = 2 - this.nbLoot();
+		for(int i = 0; i < loot ; i++) {
+			if(this.nbLoot() < 2) {
+				int x,y;
+				do {
+					x = rand.nextInt(this.plateau.length);
+					y = rand.nextInt(this.plateau[0].length);
+				}while(this.plateau[x][y].getLoot() && this.plateau[x][y].getEstPortail() && this.plateau[x][y].getEstMonstre() && this.plateau[x][y].getEstChasseur() && this.plateau[x][y].getEstPiege());
+				
+				this.plateau[x][y].changeLoot();
+			}
+		}
+	}
+	
+	public void ajoutCompetence(Personnage p) {
+		Loot.changeCompetence(p);
+	}
+	
+	/**
+	 * 
+	 * @return le nombre de tour Ã©couler depuis le debut de la partie.
+	 */
+	public int getTours() {
+		return this.tours;
+	}
+	
+	/**
+	 * Augement le nombre de tour d'un.
+	 */
+	public void addTours() {
+		this.tours++;
+	}
+	
+	/**
+	 * Rend la case dont la position lui est donnée en paramètre normale.
+	 * @param p est la position dans le terrain que l'on souhaite éditer
+	 */
+	public void setCaseNormal(Position p) {
+		this.plateau[p.getX()][p.getY()].setEstChasseur(false);
+		this.plateau[p.getX()][p.getY()].setEstMonstre(false);
+		if(this.plateau[p.getX()][p.getY()].getTypeCase() == TypeCase.LOOT) this.plateau[p.getX()][p.getY()].setLoot(false);
+	}
+	
+	/**
+	 * @return la hauteur du terrain
+	 */
+	public int getHauteur() {
+		return this.plateau.length;
+	}
+	
+	/**
+	 * @return la largeur du terrain
+	 */
+	public int getLargeur() {
+		return this.plateau[0].length;
+	}
+	
+	/**
+	 * Verifie si le chasseur a gagné la partie.
+	 * @param c est la position du chasseur
+	 * @param m est la position du monstre
+	 * @return true si le chasseur se trouve à la même position que le monstre
+	 */
+	public boolean victoireChasseur(Position c, Position m) {
+		if(c.equals(m)) return true;
+		return false;
+	}
+	
+	/**
+	 * Verifie si le monstre a gagné la partie.
+	 * @return true si le monstre a découvert toutes les cases
+	 */
+	public boolean victoireMonstre() {
+		if(this.compteurCasesDecouvertes == this.nbCases) {
+			System.out.println("Vous avez découvert toutes las cases. Vous avez GAGNÉ !");
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Verifie si le monstre a perdu la partie.
+	 * @param m c'est la position du monstre
+	 * @return true si le monstre repasse sur une case
+	 */
+	
+	public boolean defaiteMonstre(Position m) {
+		if(this.getCase(m).getEstDecouvert()) {
+			System.out.println("Vous êtes déjà passé par cette case. Vous avez PERDU !");
+			return true;
+		}
+		return false;
+	}
+	
+	
+	
+	/**
+	 * @param p est la position de la case voulue
+	 * @return la case passé en parametre
+	 */
+	public Case getCase(Position p) {
+		return this.plateau[p.getX()][p.getY()];
+	}
+	
+	/**
+	 * Permet au joueur de se téléporter d'un portail à un autre si la position passée en paramètre est la position d'un portail.
+	 * @param p est la position du joueur
+	 * @return la position d'un portail
+	 */
+	public Position teleportation(Position p) {
+		if(p.equals(this.PORTAIL1)) return this.PORTAIL2;
+		return this.PORTAIL1;
+	}
+	
+	/**
+	 * @return true si c'est au chasseur de jouer, false sinon
+	 */
+	public boolean getTourChasseur() {
+		return this.tourChasseur;
+	}
+	
+	/**
+	 * Change le statut de tourChasseur avec le booléen passé en paramètre.
+	 * @param b est le nouveau statut de tourChasseur
+	 */
+	public void setTourChasseur(boolean b){
+		this.tourChasseur = b;
+	}
+	
+	/**
+	 * @return true si c'est au monstre de jouer, false sinon
+	 */
+	public boolean getTourMonstre() {
+		return this.tourMonstre;
+	}
+	
+	/**
+	 * Change le statut de tourMonstre avec le booléen passé en paramètre. 
+	 * @param b est le nouveau statut de tourMonstre
+	 */
+	public void setTourMonstre(boolean b){
+		this.tourMonstre = b;
+	}
+	
+	/**
+	 * Découvre une case en conservant le tour de sa découverte 
+	 * @param p personnage qui devrait être un monstre
+	 */
+	public void decouvrirCase(Personnage p) {
+		if(this.plateau[p.getPosition().getX()][p.getPosition().getY()].getEstMonstre()) {
+			this.plateau[p.getPosition().getX()][p.getPosition().getY()].decouvrirCase();
+			this.plateau[p.getPosition().getX()][p.getPosition().getY()].setTempsDecouvert(this.getTours());
+		}
+	}
+	
+	/**
+	 * @return le tour auquel le dernier loot a été découvert
+	 */
+	public int getDernierLoot() {
+		return this.dernierLoot;
+	}
+	
+	/**
+	 * Remplace la valeur de dernierLoot pour le tour actuel
+	 */
+	public void setDernierLoot() {
+		this.dernierLoot = this.getTours();
+	}
+	
+	private static void clearScreen() {
+		try {
+			if(System.getProperty("os.name" ).startsWith("Windows")) {
+				Runtime.getRuntime().exec("cls" );
+			} else {
+			    Runtime.getRuntime().exec("clear" );
+			}
+		} catch(Exception e) {
+			for(int i = 0; i < 100; i++) {
+				System.out.println();
+			}
+		}
+	}
+	
+	public int getCompteurCasesDecouvertes() {
+		return this.compteurCasesDecouvertes;
+	}
+	
+	public void setCompteurCasesDecouvertes() {
+		this.compteurCasesDecouvertes++;
+	}
+	
+	public boolean contains(Position p) {
+		if(p.getX()<this.getLargeur() && p.getY()<this.getHauteur() && p.getX()>=0 && p.getY()>=0) return true;
+		else return false;
+	}
+}
