c17c22c068b17b59eaeb6735aea7c6040dba7cbf
MrKeesLer <bankaert.benoit@gmail.com>
2019-05-23 09:47:17 +0200

    Fix final (logiquement)

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -4,6 +4,7 @@ package UI;
 
 import classes.Chasseur;
 import classes.Monstre;
+import classes.Personnage;
 import classes.Plateau;
 import classes.Position;
 import javafx.application.Application;
@@ -18,8 +19,8 @@ import javafx.stage.Stage;
 public class Main extends Application{
 	
 	Plateau jeu = new Plateau();
-	Chasseur chasseur = new Chasseur(new Position(0,0));
-	Monstre monstre = new Monstre(new Position(9,9));
+	Personnage chasseur = new Chasseur(new Position(0,0));
+	Personnage monstre = new Monstre(new Position(9,9));
 	boolean tourChasseur = true;
 
 	public static void main(String[] args) {
@@ -27,7 +28,6 @@ public class Main extends Application{
 	}
 
 	public void start(Stage stage){
-		DisplayPlateau disP = new DisplayPlateau(jeu, chasseur, monstre);
 		jeu.initPlateau();
 		jeu.startPersonnage(chasseur, monstre);
 		
@@ -37,7 +37,7 @@ public class Main extends Application{
 		Canvas canvas = new Canvas(1000,1000);
 		GraphicsContext plateau = canvas.getGraphicsContext2D();
 		
-		disP.affichagePlateauVisionChasseur(plateau);
+		affichagePlateauVisionChasseur(plateau);
 		
 		pane.getChildren().add(canvas);
 		
@@ -48,24 +48,22 @@ public class Main extends Application{
 				if(KeyEvent.KEY_PRESSED != null) {
 					if(tourChasseur) {
 						chasseur.estDeplace(jeu, event.getCode().toString());
-						disP.affichagePlateauVisionChasseur(plateau);
-						chasseur.setDeplacement(chasseur.getDeplacement()-jeu.getCase(chasseur.getPosition()).getTypeTerrain().getDeplacement());
-						System.out.println(chasseur.getDeplacement());
-						if(chasseur.getDeplacement() <= 0) {
+						affichagePlateauVisionChasseur(plateau);
+						System.out.println(chasseur.getDeplacement()+" "+chasseur.getPosition());
+						if(chasseur.getDeplacement() == 0) {
 							chasseur.resetMouvement();
 							tourChasseur = false;
-							disP.affichagePlateauVisionMonstre(plateau);
+							affichagePlateauVisionMonstre(plateau);
 						}
-						
 					} else {
 						monstre.estDeplace(jeu, event.getCode().toString());
-						disP.affichagePlateauVisionMonstre(plateau);
+						affichagePlateauVisionMonstre(plateau);
 						monstre.setDeplacement(monstre.getDeplacement()-jeu.getCase(monstre.getPosition()).getTypeTerrain().getDeplacement());
 						System.out.println(monstre.getDeplacement());
 						if(monstre.getDeplacement() <= 0) {
 							monstre.resetMouvement();
 							tourChasseur = true;
-							disP.affichagePlateauVisionChasseur(plateau);
+							affichagePlateauVisionChasseur(plateau);
 						}
 					}	
 				}
@@ -81,6 +79,89 @@ public class Main extends Application{
 		
 	}
 	
+	public void affichagePlateau(GraphicsContext p) {
+		p.clearRect(0, 0, p.getCanvas().getWidth(), p.getCanvas().getHeight());
+		for(int i=0; i<jeu.getLargeur(); i++) {
+			for(int j=0; j<jeu.getHauteur(); j++) {
+				
+				p.drawImage(jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible(),
+						((9-j))*(jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible().getWidth()/2)+(i*jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible().getWidth()/2),
+						  j*jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible().getHeight()/6+(i*jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible().getHeight()/6));
+				
+				if(jeu.getPlateau()[i][j].getEstChasseur()) {
+					p.drawImage(chasseur.getImage(),
+							((9-j))*(chasseur.getImage().getWidth()/2)+(i*chasseur.getImage().getWidth()/2),
+							  j*chasseur.getImage().getHeight()/6+(i*chasseur.getImage().getHeight()/6));
+				}
+				if(jeu.getPlateau()[i][j].getEstMonstre()) {
+					p.drawImage(monstre.getImage(),
+							((9-j))*(monstre.getImage().getWidth()/2)+(i*monstre.getImage().getWidth()/2),
+							  j*monstre.getImage().getHeight()/6+(i*monstre.getImage().getHeight()/6));
+				}
+			}
+		}
+	}
 	
+	public void affichagePlateauVisionChasseur(GraphicsContext p) {
+		p.clearRect(0, 0, p.getCanvas().getWidth(), p.getCanvas().getHeight());
+		for(int i=0; i<jeu.getLargeur(); i++) {
+			for(int j=0; j<jeu.getHauteur(); j++) {
+				if(i>chasseur.getPosition().getX()-jeu.getCase(chasseur.getPosition()).getTypeTerrain().getVision()
+						&& i<chasseur.getPosition().getX()+jeu.getCase(chasseur.getPosition()).getTypeTerrain().getVision()
+						&& j>chasseur.getPosition().getY()-jeu.getCase(chasseur.getPosition()).getTypeTerrain().getVision()
+						&& j<chasseur.getPosition().getY()+jeu.getCase(chasseur.getPosition()).getTypeTerrain().getVision()) {
+					p.drawImage(jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible(),
+							((9-j))*(jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible().getWidth()/2)+(i*jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible().getWidth()/2),
+							  j*jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible().getHeight()/6+(i*jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible().getHeight()/6));
+					
+					if(jeu.getPlateau()[i][j].getEstChasseur()) {
+						p.drawImage(chasseur.getImage(),
+								((9-j))*(chasseur.getImage().getWidth()/2)+(i*chasseur.getImage().getWidth()/2),
+								  j*chasseur.getImage().getHeight()/6+(i*chasseur.getImage().getHeight()/6));
+					}
+					if(jeu.getPlateau()[i][j].getEstMonstre()) {
+						p.drawImage(monstre.getImage(),
+								((9-j))*(monstre.getImage().getWidth()/2)+(i*monstre.getImage().getWidth()/2),
+								  j*monstre.getImage().getHeight()/6+(i*monstre.getImage().getHeight()/6));
+					}
+				} else {
+					p.drawImage(jeu.getPlateau()[i][j].getTypeTerrain().getImageNonVisible(),
+							((9-j))*(jeu.getPlateau()[i][j].getTypeTerrain().getImageNonVisible().getWidth()/2)+(i*jeu.getPlateau()[i][j].getTypeTerrain().getImageNonVisible().getWidth()/2),
+							  j*jeu.getPlateau()[i][j].getTypeTerrain().getImageNonVisible().getHeight()/6+(i*jeu.getPlateau()[i][j].getTypeTerrain().getImageNonVisible().getHeight()/6));
+				}
+			}
+		}
+	}
+	
+	public void affichagePlateauVisionMonstre(GraphicsContext p) {
+		p.clearRect(0, 0, p.getCanvas().getWidth(), p.getCanvas().getHeight());
+		for(int i=0; i<jeu.getLargeur(); i++) {
+			for(int j=0; j<jeu.getHauteur(); j++) {
+				if(i>monstre.getPosition().getX()-jeu.getCase(monstre.getPosition()).getTypeTerrain().getVision()
+						&& i<monstre.getPosition().getX()+jeu.getCase(monstre.getPosition()).getTypeTerrain().getVision()
+						&& j>monstre.getPosition().getY()-jeu.getCase(monstre.getPosition()).getTypeTerrain().getVision()
+						&& j<monstre.getPosition().getY()+jeu.getCase(monstre.getPosition()).getTypeTerrain().getVision()) {
+					p.drawImage(jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible(),
+							((9-j))*(jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible().getWidth()/2)+(i*jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible().getWidth()/2),
+							  j*jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible().getHeight()/6+(i*jeu.getPlateau()[i][j].getTypeTerrain().getImageVisible().getHeight()/6));
+					
+					if(jeu.getPlateau()[i][j].getEstChasseur()) {
+						p.drawImage(chasseur.getImage(),
+								((9-j))*(chasseur.getImage().getWidth()/2)+(i*chasseur.getImage().getWidth()/2),
+								  j*chasseur.getImage().getHeight()/6+(i*chasseur.getImage().getHeight()/6));
+					}
+					if(jeu.getPlateau()[i][j].getEstMonstre()) {
+						p.drawImage(monstre.getImage(),
+								((9-j))*(monstre.getImage().getWidth()/2)+(i*monstre.getImage().getWidth()/2),
+								  j*monstre.getImage().getHeight()/6+(i*monstre.getImage().getHeight()/6));
+					}
+				} else {
+					p.drawImage(jeu.getPlateau()[i][j].getTypeTerrain().getImageNonVisible(),
+							((9-j))*(jeu.getPlateau()[i][j].getTypeTerrain().getImageNonVisible().getWidth()/2)+(i*jeu.getPlateau()[i][j].getTypeTerrain().getImageNonVisible().getWidth()/2),
+							  j*jeu.getPlateau()[i][j].getTypeTerrain().getImageNonVisible().getHeight()/6+(i*jeu.getPlateau()[i][j].getTypeTerrain().getImageNonVisible().getHeight()/6));
+				}
+			}
+		}
+	}
 	
 }
