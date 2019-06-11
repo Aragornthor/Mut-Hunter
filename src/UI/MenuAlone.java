5909d64f66d32688e5efc1f9b8be3cc914947283
Ataron <ataron55@gmail.com>
2019-06-11 18:54:18 +0200

    Léger fix

--- src/UI/MenuAlone.java
+++ src/UI/MenuAlone.java
@@ -8,7 +8,6 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Separator;
